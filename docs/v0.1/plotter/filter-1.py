413fff9234faacee537ce3e47472c90d02e5dc05
Duncan Macleod <duncan.macleod@ligo.org>
2014-08-12 18:08:29 -0400

    v0.1a9: updated docs to new pre-release version

--- docs/v0.1/plotter/filter-1.py
+++ docs/v0.1/plotter/filter-1.py
@@ -1,4 +1,5 @@
 from scipy import signal
 from gwpy.plotter import BodePlot
-highpass = signal.butter(4, 10 * (2.0 / 256), btype='highpass')
-plot = BodePlot(highpass, sample_rate=256)
+highpass = signal.butter(4, 10 * (2. * signal.np.pi), btype='highpass',
+# analog=True)
+plot = BodePlot(highpass)
