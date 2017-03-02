430fb2c5981b3ea8918485019ba8b7cce58bfafa
gwpybot <gwpybot@gmail.com>
2017-03-02 13:39:02 -0800

    nightly docs update

--- docs/latest/examples/table/rate_binned-1.py
+++ docs/latest/examples/table/rate_binned-1.py
@@ -1,4 +1,4 @@
 from gwpy.table import EventTable
 events = EventTable.read(
-    '../../gwpy/tests/data/H1-LDAS_STRAIN-968654552-10.xml.gz',
-    format='ligolw.sngl_burst', columns=['time', 'snr'])
+    'H1-LDAS_STRAIN-968654552-10.xml.gz', format='ligolw.sngl_burst',
+    columns=['time', 'snr'])
