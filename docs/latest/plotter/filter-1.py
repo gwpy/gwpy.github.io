6c1e1b5103c093c8f6674a8c24cacb6b629e892d
gwpybot <gwpybot@gmail.com>
2018-01-05 08:07:39 -0800

    nightly docs update

--- docs/latest/plotter/filter-1.py
+++ docs/latest/plotter/filter-1.py
@@ -1,4 +1,4 @@
-from gwpy.signal import bandpass
+from gwpy.signal.filter_design import bandpass
 from gwpy.plotter import BodePlot
 zpk = bandpass(40, 1000, 4096, analog=True)
 plot = BodePlot(zpk, analog=True, title='40-1000\,Hz bandpass filter')
