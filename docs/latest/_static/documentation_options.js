1c28f9be62b7838ad81434be4d8db882bc40ec62
gwpybot <gwpybot@gmail.com>
2019-01-30 09:20:38 -0800

    nightly docs update

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.2+262.g10a5354d',
+    VERSION: '0.12.2+268.gc4bb40f8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
