44f7cc516ac19b733ad23ff2b1092b9d6ffd9dcf
gwpybot <gwpybot@gmail.com>
2019-02-28 00:33:03 -0800

    nightly docs update

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.2+347.g2f660847',
+    VERSION: '0.12.2+405.gc7ab56bb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- docs/latest/examples/timeseries/qscan-3.py
+++ docs/latest/examples/timeseries/qscan-3.py
@@ -1,4 +1,4 @@
-plot = qspecgram.crop(1126259462.2, 1126259462.5).plot(figsize=[8, 4])
+plot = qspecgram.plot(figsize=[8, 4])
 ax = plot.gca()
 ax.set_xscale('seconds')
 ax.set_yscale('log')
