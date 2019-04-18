e1b92268fc4e658ade3c97a5eb47e7cea0f75ac4
gwpybot <gwpybot@gmail.com>
2019-04-18 08:33:02 -0700

    nightly docs update

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.2+513.gd9db333e',
+    VERSION: '0.12.2+524.g4f96e206',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- docs/latest/examples/timeseries/inject-2.py
+++ docs/latest/examples/timeseries/inject-2.py
@@ -1,6 +1,6 @@
 from astropy.utils.data import get_readable_fileobj
-source = 'https://losc.ligo.org/s/events/GW150914/P150914/'
-url = '%s/fig2-unfiltered-waveform-H.txt' % source
+url = ("https://www.gw-openscience.org/s/events/GW150914/P150914/"
+       "fig2-unfiltered-waveform-H.txt")
 with get_readable_fileobj(url) as f:
     signal = TimeSeries.read(f, format='txt')
 signal.t0 = .5  # make sure this intersects with noise time samples
