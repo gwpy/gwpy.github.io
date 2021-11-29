75755ceb7e9529a27de625f0acc35eb6dd68c6a5
gwpybot <gwpybot@gmail.com>
2021-11-29 06:53:56 -0800

    nightly docs update

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.1.dev41+g473689b8',
+    VERSION: '2.1.1.dev62+gd55c8e6e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
