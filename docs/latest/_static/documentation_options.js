c4d1b265ea319b7cb8bd792359d35a9faf138be9
gwpybot <gwpybot@gmail.com>
2025-05-17 00:13:59 -0700

    nightly docs update

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.1.0.dev760+gddc72f63',
+    VERSION: '3.1.0.dev763+g87045365',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
